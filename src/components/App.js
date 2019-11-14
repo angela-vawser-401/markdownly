ae0eb05ac01e267bd24454789e19d23b315ee8e4
Luke Donahue <luke9389@gmail.com>
2019-11-14 10:07:16 -0800

    styling

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,8 @@ import styles from './Main.css';
 export default function App() {
   return (
     <>
-      <h1 className={styles.Main}>Markdown Editor</h1>
-      <Document />
+      <h1 className={styles.Header}>Markdown Editor</h1>
+      <Document className={styles.Main}/>
     </>
   );
 }
