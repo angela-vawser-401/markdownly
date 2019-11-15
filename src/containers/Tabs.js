6d85513144df8cff9483bcd3b15e62f858a3a20e
Angela Vawser <amvawser@gmail.com>
2019-11-15 15:14:34 -0800

    hooks for Tabs added and working

--- src/containers/Tabs.js
+++ src/containers/Tabs.js
@@ -1,35 +1,22 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 // import PropTypes from 'prop-types';
 import TabList from '../components/markdown/TabList';
 import { setActiveTab, deleteTab, addTab } from '../actions/editorActions';
 import { getMarkdownArray, getActiveIndex } from '../selector/markdownSelector';
 
 // eslint-disable-next-line react/prop-types
-const Tabs = ({ activeIndex, list, handleTabClick, handleTabClose, handleAddTab }) => {
+const Tabs = () => {
+  const dispatch = useDispatch();
+  const handleTabClick = (num) => dispatch(setActiveTab(num));
+  const handleTabClose = (num) => dispatch(deleteTab(num));
+  const handleAddTab = () => dispatch(addTab());
+  const activeIndex = useSelector(getActiveIndex);
+  const list = useSelector(getMarkdownArray);
+
   return (
     <TabList list={list} activeIndex={activeIndex} handleTabClick={handleTabClick} handleTabClose={handleTabClose} handleAddTab={handleAddTab}/>
   );
 };
 
-const mapStateToProps = state => ({
-  activeIndex: getActiveIndex(state),
-  list: getMarkdownArray(state)
-});
-
-const mapDispatchToProps = dispatch => ({
-  handleTabClick(num) {
-    dispatch(setActiveTab(num));
-  },
-  handleTabClose(num) {
-    dispatch(deleteTab(num));
-  },
-  handleAddTab(){
-    dispatch(addTab());
-  }
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Tabs);
+export default Tabs;
