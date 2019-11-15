d1bbba10df1340f1a578739a111cb4a3566a5ac0
Kamran Ali <kgali21@gmail.com>
2019-11-15 15:08:17 -0800

    tabs container refactored

--- src/containers/Tabs.js
+++ src/containers/Tabs.js
@@ -1,35 +1,18 @@
 import React from 'react';
-import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
 import TabList from '../components/markdown/TabList';
 import { setActiveTab, deleteTab, addTab } from '../actions/editorActions';
 import { getMarkdownArray, getActiveIndex } from '../selector/markdownSelector';
 
-// eslint-disable-next-line react/prop-types
-const Tabs = ({ activeIndex, list, handleTabClick, handleTabClose, handleAddTab }) => {
+export default function Tabs() {
+  const dispatch = useDispatch();
+  const handleTabClick = num => dispatch(setActiveTab(num));
+  const handleTabClose = num => dispatch(deleteTab(num));
+  const handleAddTab = () => dispatch(addTab());
+  const list = useSelector(getMarkdownArray);
+  const activeIndex = useSelector(getActiveIndex);
+
   return (
-    <TabList list={list} activeIndex={activeIndex} handleTabClick={handleTabClick} handleTabClose={handleTabClose} handleAddTab={handleAddTab}/>
+    <TabList list={list} activeIndex={activeIndex} handleTabClick={handleTabClick} handleTabClose={handleTabClose} handleAddTab={handleAddTab} />
   );
-};
-
-const mapStateToProps = state => ({
-  activeIndex: getActiveIndex(state),
-  list: getMarkdownArray(state)
-});
-
-const mapDispatchToProps = dispatch => ({
-  handleTabClick(num) {
-    dispatch(setActiveTab(num));
-  },
-  handleTabClose(num) {
-    dispatch(deleteTab(num));
-  },
-  handleAddTab(){
-    dispatch(addTab());
-  }
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Tabs);
+}
