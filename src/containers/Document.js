b58944ad0e622cf48acb66443fc9b397a44caa9e
Kamran Ali <kgali21@gmail.com>
2019-11-15 16:31:05 -0800

    containers are refactored with hooks

--- src/containers/Document.js
+++ src/containers/Document.js
@@ -1,39 +1,27 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Preview from '../components/markdown/Preview';
 import Editor from '../components/markdown/Editor';
 import styles from './Document.css';
 import { updateMarkdown } from '../actions/editorActions';
 import { getActiveMarkdown } from '../selector/markdownSelector';
 
-// eslint-disable-next-line react/prop-types
-const Document = ({ activeMarkdown, handleMarkdownChange }) => {
+export default function Document() {
+  const dispatch = useDispatch();
+  const handleMarkdownChange = ({ target }) => dispatch(updateMarkdown(target.value));
+  const activeMarkdown = useSelector(getActiveMarkdown);
+
   return (
     <>
-      <div className={styles.Document}>
-        <Editor
-          activeMarkdown={activeMarkdown}
-          handleMarkdownChange={handleMarkdownChange}
-        />
-        <Preview
-          activeMarkdown={activeMarkdown}
-        />
-      </div>
+    <div className={styles.Document}>
+      <Editor
+        activeMarkdown={activeMarkdown}
+        handleMarkdownChange={handleMarkdownChange}
+      />
+      <Preview
+        activeMarkdown={activeMarkdown}
+      />
+    </div>
     </>
   );
-};
-
-const mapDispatchToProps = dispatch => ({
-  handleMarkdownChange({ target }) {
-    dispatch(updateMarkdown(target.value));
-  }
-});
-
-const mapStateToProps = state => ({
-  activeMarkdown: getActiveMarkdown(state)
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Document);
+}
--- src/containers/SaveContainer.js
+++ src/containers/SaveContainer.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Save from '../components/markdown/Save';
 import { saveMarkdown } from '../actions/pageActions';
 
-// eslint-disable-next-line react/prop-types
-const SaveContainer = ({ handleSave, markdown }) => {
+export default function SaveContainer() {
+  const dispatch = useDispatch();
+  const handleSave = markdown => dispatch(saveMarkdown(markdown));
+  const markdown = useSelector(saveMarkdown);
+
+  // eslint-disable-next-line react/prop-types
   return (
     <Save handleSave={() => { handleSave(markdown); }} />
   );
-};
-
-const mapDispatchToProps = dispatch => ({
-  handleSave(markdown) {
-    dispatch(saveMarkdown(markdown));
-  }
-});
-
-const mapStateToProps = state => ({
-  markdown: state.editor
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SaveContainer);
+}
